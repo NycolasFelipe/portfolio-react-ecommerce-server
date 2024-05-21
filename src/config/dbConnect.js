2b50f59e5fe14ee85e29f52138162938086322ac
Nycolas Felipe <nycolaslourenco.contato@gmail.com>
2024-05-21 15:18:49 -0300

    better environment variables name pattern

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -2,12 +2,12 @@ const mysql = require('mysql2');
 require("dotenv").config();
 
 const db = mysql.createConnection({
-  database: "products",
-  uri: process.env.URI,
-  host: process.env.HOST,
-  port: process.env.SERVER_PORT,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
+  database: process.env.DB_NAME,
+  uri: process.env.DB_URI,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
 });
 
 module.exports = db;
