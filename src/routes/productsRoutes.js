96f87f23b302cd52a5ed129f0be9cd1cb700fb49
Nycolas Felipe <nycolaslourenco.contato@gmail.com>
2024-05-30 23:29:49 -0300

    teste auth jwt

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,18 +1,11 @@
 const express = require("express");
-const { auth } = require("express-oauth2-jwt-bearer");
-const ProductsController = require("../controllers/productsController");
-
-const checkJwt = auth({
-  audience: 'https://portfolio-react-ecommerce-server.vercel.app/',
-  issuerBaseURL: 'https://ecommerce-web.us.auth0.com/',
-  tokenSigningAlg: 'RS256'
-});
+const ProductsController = require("../controllers/productsController.js");
 
 const products = express.Router();
 
 products
-  .get("/products", checkJwt, ProductsController.getProducts)
-  .get("/products/:id", checkJwt, ProductsController.getProductsById)
+  .get("/products", ProductsController.getProducts)
+  .get("/products/:id", ProductsController.getProductsById)
   .get("/products/:id/detail", ProductsController.getProductDetailById)
   .get("/products/category/:category", ProductsController.getProductsByCategory);
 
