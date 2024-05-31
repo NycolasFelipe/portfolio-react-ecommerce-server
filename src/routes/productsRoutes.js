b1208c2c3a407304413068b1bd14a935efef58ad
Nycolas Felipe <nycolaslourenco.contato@gmail.com>
2024-05-30 23:20:04 -0300

    teste auth jwt

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,11 +1,17 @@
 const express = require("express");
-const ProductsController = require("../controllers/productsController.js");
+const { auth } = require("express-oauth2-jwt-bearer");
+const ProductsController = require("../controllers/productsController");
+
+const checkJwt = auth({
+  audience: 'ecommerce-user',
+  issuerBaseURL: `https://ecommerce-web.us.auth0.com/`,
+});
 
 const products = express.Router();
 
 products
-  .get("/products", ProductsController.getProducts)
-  .get("/products/:id", ProductsController.getProductsById)
+  .get("/products", checkJwt, ProductsController.getProducts)
+  .get("/products/:id", checkJwt, ProductsController.getProductsById)
   .get("/products/:id/detail", ProductsController.getProductDetailById)
   .get("/products/category/:category", ProductsController.getProductsByCategory);
 
